5bf7fe9cc6c67a3b7767900d13497bb46a926f5d
Sayposada <siposadaa@unal.edu.co>
2024-02-20 09:17:42 -0500

    si

---
+++ Untitled-3.py
@@ -0,0 +1,67 @@
+#Creamos las clases 
+
+#auto
+#asiento
+class Asiento:
+  def __init__(self, color, precio, registro):
+    self.color = color
+    self.precio = precio
+    self.registro = registro
+
+  #método
+  def cambiarColor(self, color):
+    if(color == "rojo" or color== "verde" or color == "amarillo" or color == "negro" or color == "blanco"):
+      self.color = color
+
+#motor 
+
+class Auto:
+  cantidadCreados = 0
+  def __init__ (self, modelo, precio, asientos, marca, motor, registro):
+    self.modelo = modelo
+    self.precio = precio
+    self.asientos = asientos
+    self.marca = marca
+    self.motor = motor
+    self.registro = registro
+    
+  def cantidadAsientos (self):
+    numAsientos = 0
+    for asiento in self.asientos:
+      if (type(asiento) == Asiento):
+        numAsientos += 1
+    return numAsientos
+
+  def verificarIntegridad(self):
+    if (self.motor.registro == self.registro):
+      for asiento in self.asientos:
+        if (type(asiento) == Asiento):
+          if self.registro!=asiento.registro:
+            return "Las piezas no son originales"
+        return "Auto original"
+    else:
+      return "Las piezas no son originales"
+
+class Motor:
+  def __init__(self, numeroCilindros, tipo, registro):
+    self.numeroCilindros = numeroCilindros
+    self.tipo= tipo
+    self.registro= registro
+
+    # metodos
+
+  def cambiarRegistro(self, registro):
+    self.registro = registro
+
+  def asignarTipo(self, tipo):
+    if (tipo == "electrico" or tipo == "gasolina"):
+      self.tipo = tipo
+
+
+a1 = Auto("model 3", 33000, [Asiento("blanco", 5000, 32),None, None, Asiento("blanco", 5000, 32), None],
+          "tesla", Motor(4, "electrico", 32), 32)
+a2 = Auto("model 3", 33000, [Asiento("blanco", 5000, 40),None, None, Asiento("blanco", 5000, 32), None],
+"tesla", Motor(4, "electrico", 32), 32)
+
+print(a1.verificarIntegridad())
+print(a2.verificarIntegridad())
